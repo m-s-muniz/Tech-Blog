ff5c9d8a4d12e25f2a5d5bac299b1ba673052e3d
Michael Muniz <m.s.muniz@sbcglobal.net>
2023-05-03 02:51:56 -0500

    Added boilerplate and skeleton  files to set  up login and registration. Setup api homeRoutes and userRoutes. Made schema.sql file to create database. Setup User model. Setup seeds for some sample data for the db tables.  Setup some hanblebars templets in views and layouts.

---
+++ controllers/api/blogRoutes.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+const { Blog } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+
+
+
+
+module.exports = router;
+
---
+++ models/Blog.js
@@ -0,0 +1,8 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Blog extends Model {}
+
+
+
+module.exports = Blog;
